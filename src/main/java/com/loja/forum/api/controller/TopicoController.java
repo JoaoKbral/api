8654c8422ddd1b59ba866c6c11d2cc8c7583d684
João Rodolpho <joaorodolphocabral@gmail.com>
2023-11-23 11:48:13 -0300

    add topics list

--- src/main/java/com/loja/forum/api/controller/TopicoController.java
+++ src/main/java/com/loja/forum/api/controller/TopicoController.java
@@ -1,6 +1,9 @@
 package com.loja.forum.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +18,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 import com.loja.forum.api.dao.DadosAtualizarTopico;
 import com.loja.forum.api.dao.DadosCadastroTopico;
 import com.loja.forum.api.dao.DadosDetalhamentoTopico;
+import com.loja.forum.api.dao.DadosListagemTopico;
 import com.loja.forum.api.models.Topico;
 import com.loja.forum.api.repositories.TopicoRepository;
 
@@ -59,6 +63,11 @@ public class TopicoController {
         var topico = topicoRepository.getReferenceById(id);
 
         return ResponseEntity.ok(new DadosDetalhamentoTopico(topico));
+    }
+
+    public ResponseEntity<Page<DadosListagemTopico>> listar(@PageableDefault (size = 10) Pageable pageable){
+        var page = topicoRepository.findAll(pageable).map(DadosListagemTopico::new);
+        return ResponseEntity.ok(page);
     }
 
 }
---
+++ src/main/java/com/loja/forum/api/dao/DadosListagemTopico.java
@@ -0,0 +1,21 @@
+package com.loja.forum.api.dao;
+
+import java.time.LocalDate;
+
+import com.loja.forum.api.models.Curso;
+import com.loja.forum.api.models.StatusTopico;
+import com.loja.forum.api.models.Topico;
+import com.loja.forum.api.models.Usuario;
+
+public record DadosListagemTopico(String titulo,
+        String mensagem,
+        LocalDate dataCriacao,
+        StatusTopico status,
+        Usuario usuario,
+        Curso curso) {
+
+    public DadosListagemTopico(Topico topico) {
+        this(topico.getTitulo(), topico.getMensagem(), topico.getDataCriacao(), topico.getStatus(),
+                topico.getUsuarioId(), topico.getCurso());
+    }
+}
